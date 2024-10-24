7243df5bae4e53e5bdc28fc4f93ae8ad2bcc6da7
inegohe <ikhosler@gmail.com>
2024-10-24 18:21:58 +0300

    finish the job details screen

--- app/index.js
+++ app/index.js
@@ -1,45 +1,60 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { View, ScrollView, SafeAreaView } from "react-native";
-import { Stack, useRouter } from 'expo-router';
+import { Stack, useRouter } from "expo-router";
 
-import { COLORS, icons, images, SIZES } from '../constants';
-import { Welcome, Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welocome } from '../components';
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Welcome,
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welocome,
+} from "../components";
 
 const Home = () => {
-    const router = useRouter();
+  const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState("");
 
-    return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-            <Stack.Screen 
-            options= {{
-                headerStyle: { backgroundColor: COLORS.lightWhite },
-                headerShadowVisible: false,
-                headerLeft: () => (
-                    <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
-                ),
-                headerRight: () => (
-                    <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
-                ),
-                headerTitle: ''
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+
+      <ScrollView showVerticleScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              if (searchTerm) {
+                router.push(`search/${searchTerm}`);
+              }
             }}
-            />
+          />
 
-            <ScrollView showVerticleScrollIndicator={false}>
-                <View
-                style={{
-                    flex: 1,
-                    padding: SIZES.medium
-                }}
-                >
-                    <Welcome />
+          <Popularjobs />
 
-                    <Popularjobs />
-                    
-                    <Nearbyjobs />
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    )
-}
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default Home;
--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -4,6 +4,7 @@ import {
   ScrollView,
   ActivityIndicator,
   RefreshControl,
+  View,
 } from "react-native";
 import { Stack, useRouter, useLocalSearchParams } from "expo-router";
 import { useCallback, useState } from "react";
@@ -18,7 +19,8 @@ import {
 } from "../../components";
 import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
-import { View } from "react-native-web";
+
+const tabs = ["About", "Qualification", "Responsibilities"];
 
 const JobDetails = () => {
   const params = useLocalSearchParams();
@@ -29,8 +31,32 @@ const JobDetails = () => {
   });
 
   const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+
+  //console.log(data[0])
 
   const onRefresh = () => {};
+
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return <Specifics
+        title='Qualification'
+        points={data[0].job_highlights?.Qualifications ?? ['N/A']}
+        />
+      case "About":
+        return <JobAbout 
+        info={data[0].job_description ?? 'No data provided'}
+        />
+      case "Responsibilities":
+        return <Specifics
+        title='Responsibilities'
+        points={data[0].job_highlights?.Responsibilities ?? ['N/A']}
+        />
+      default:
+        break;
+    }
+  };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -60,7 +86,7 @@ const JobDetails = () => {
             <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
           }
         >
-          { isLoading ? (
+          {isLoading ? (
             <ActivityIndicator size="large" color={COLORS.primary} />
           ) : error ? (
             <Text>Something went wrong</Text>
@@ -75,10 +101,18 @@ const JobDetails = () => {
                 Location={data[0].job_country}
               />
 
-              <JobTabs />
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+
+              {displayTabContent()}
             </View>
           )}
         </ScrollView>
+
+        <JobFooter url={data[0]?.job_google_link ?? 'http://careers.google.com/jobs/results'}/>
       </>
     </SafeAreaView>
   );
